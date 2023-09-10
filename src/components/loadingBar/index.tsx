8dc1bd00e49f73fa93d2e25cec7d9cbe34158578
Emam546 <emam54637@gmail.com>
2023-09-10 19:31:41 +0300

    add loading bar

---
+++ src/components/loadingBar/index.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import style from "./style.module.scss";
+import classNames from "classnames";
+export default function LoadingBar() {
+    const router = useRouter();
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const handleStart = (url: string) =>
+            url !== router.asPath && setLoading(true);
+        const handleComplete = () => setLoading(false);
+
+        router.events.on("routeChangeStart", handleStart);
+        router.events.on("routeChangeComplete", handleComplete);
+        router.events.on("routeChangeError", handleComplete);
+
+        return () => {
+            router.events.off("routeChangeStart", handleStart);
+            router.events.off("routeChangeComplete", handleComplete);
+            router.events.off("routeChangeError", handleComplete);
+        };
+    }, []);
+    if (!loading) return null;
+    return (
+        <div
+            className={classNames(
+                style["loading-bar"],
+                "fixed top-0 left-0 w-full h-[4px] z-[10000]"
+            )}
+        >
+            <div className="h-full"></div>
+        </div>
+    );
+}
