aa2bf3f51421132ae983e19061899a8edd1b26b1
Emam546 <emam54637@gmail.com>
2023-12-22 15:20:59 +0200

    adding storage function

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -9,6 +9,11 @@ import {
     connectFirestoreEmulator,
 } from "firebase/firestore";
 import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
+import {
+    getStorage,
+    ref as storageRef,
+    connectStorageEmulator,
+} from "firebase/storage";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 let firebaseConfig: FirebaseOptions = {
     apiKey: "AIzaSyDuI2roFqIlzYsbqvU8EiYrGTWzK4pym7Y",
@@ -23,12 +28,14 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const fireStore = getFirestore(app);
 export const functions = getFunctions(app);
+export const storage = getStorage(app);
 if (process.env.NODE_ENV == "development") {
     firebaseConfig.authDomain = "localhost";
     // firebaseConfig.apiKey = 'YOUR_API_KEY';
     connectFirestoreEmulator(fireStore, "127.0.0.1", 8080);
     connectAuthEmulator(auth, "http://127.0.0.1:9099");
     connectFunctionsEmulator(functions, "127.0.0.1", 5001);
+    connectStorageEmulator(storage, "127.0.0.1", 9199);
 }
 // export const analytics = getAnalytics(app);
 export function createCollection<T extends keyof DataBase>(path: T) {
@@ -41,4 +48,7 @@ export function getDocRef<T extends keyof DataBase>(
     return doc(fireStore, path, ...pathFragments) as DocumentReference<
         DataBase[T]
     >;
+}
+export function getStorageRef(...paths: string[]) {
+    return storageRef(storage, paths.join("/"));
 }
