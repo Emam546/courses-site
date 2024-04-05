cb06b9689c08c49bc6e462ae47130c89ebb26e6a
Emam546 <emam54637@gmail.com>
2024-04-05 14:00:26 +0200

    fix dragelement on mobile screen

--- src/components/InsertCommonData/Elem.tsx
+++ src/components/InsertCommonData/Elem.tsx
@@ -2,7 +2,7 @@ import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSyncRefs } from "@/hooks";
 import classNames from "classnames";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import DraggableComp from "@/components/common/drag";
 import React from "react";
 import { PrimaryProps } from "./EleGen";
@@ -30,59 +30,72 @@ export const Elem = React.forwardRef<HTMLDivElement, DraggableItem>(
         const [drag, setDrag] = useState(false);
         const [parentDiv, setParentDiv] = useState<HTMLDivElement | null>(null);
         const allRefs = useSyncRefs(setParentDiv, ref);
-
+        const containerRef = useRef<HTMLDivElement>(null);
+        useEffect(() => {
+            if (!containerRef.current) return;
+            if (drag)
+                containerRef.current.style.height = `${parentDiv?.clientHeight}px`;
+            else containerRef.current.style.height = `fit-content`;
+        }, [drag]);
         return (
             <div
-                ref={allRefs}
-                className={classNames(
-                    {
-                        "tw-fixed tw-z-50 tw-flex-1": drag,
-                        static: !drag,
-                    },
-                    "tw-flex-1"
-                )}
+                ref={containerRef}
+                className={classNames({
+                    "tw-border-2 tw-border-dashed tw-border-black/30 tw-p-3": drag,
+                })}
             >
-                <div className="tw-relative tw-cursor-pointer tw-flex tw-items-center tw-justify-between tw-gap-x-3">
-                    {!noDragging && (
-                        <DraggableComp
-                            onDragStart={() => {
-                                setDrag(true);
-                                if (onDragStart && parentDiv)
-                                    onDragStart(parentDiv);
-                            }}
-                            onDragOver={() => {
-                                setDrag(false);
-                                if (onDragOver && parentDiv)
-                                    onDragOver(parentDiv);
-                            }}
-                            onDrag={function (ev) {
-                                if (onDrag && parentDiv)
-                                    onDrag.call(parentDiv, ev);
-                            }}
-                            parentDiv={parentDiv}
-                        />
+                <div
+                    ref={allRefs}
+                    className={classNames(
+                        {
+                            "tw-fixed tw-z-50 tw-flex-1": drag,
+                            static: !drag,
+                        },
+                        "tw-flex-1"
                     )}
-
-                    <div className="tw-flex-1">{children}</div>
-                    <div className="tw-flex tw-items-center tw-gap-x-3 tw-select-none">
-                        {loading && (
-                            <span aria-label="delete">
-                                <CircularProgress className="max-w-[1.2rem] max-h-[1.2rem]" />
-                            </span>
-                        )}
-                        {deleteSelf && (
-                            <button
-                                type="button"
-                                className="hover:tw-text-red-600 disabled:tw-text-neutral-600 tw-border-0 tw-bg-transparent"
-                                onClick={() => {
-                                    if (parentDiv) deleteSelf.call(parentDiv);
+                >
+                    <div className="tw-relative tw-cursor-pointer tw-flex tw-items-center tw-justify-between tw-gap-x-3">
+                        {!noDragging && (
+                            <DraggableComp
+                                onDragStart={() => {
+                                    setDrag(true);
+                                    if (onDragStart && parentDiv)
+                                        onDragStart(parentDiv);
                                 }}
-                                aria-label="delete"
-                                disabled={disabled}
-                            >
-                                <FontAwesomeIcon icon={faTrashCan} />
-                            </button>
+                                onDragOver={() => {
+                                    setDrag(false);
+                                    if (onDragOver && parentDiv)
+                                        onDragOver(parentDiv);
+                                }}
+                                onDrag={function (ev) {
+                                    if (onDrag && parentDiv)
+                                        onDrag.call(parentDiv, ev);
+                                }}
+                                parentDiv={parentDiv}
+                            />
                         )}
+
+                        <div className="tw-flex-1">{children}</div>
+                        <div className="tw-flex tw-items-center tw-gap-x-3 tw-select-none">
+                            {loading && (
+                                <span aria-label="delete">
+                                    <CircularProgress className="max-w-[1.2rem] max-h-[1.2rem]" />
+                                </span>
+                            )}
+                            {deleteSelf && (
+                                <button
+                                    type="button"
+                                    className="hover:tw-text-red-600 disabled:tw-text-neutral-600 tw-border-0 tw-bg-transparent"
+                                    onClick={() => {
+                                    if (parentDiv) deleteSelf.call(parentDiv);
+                                    }}
+                                    aria-label="delete"
+                                    disabled={disabled}
+                                >
+                                    <FontAwesomeIcon icon={faTrashCan} />
+                                </button>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
